5fc93d121e73c3b14c8b0286e421f3a5ee627efd
shivam nishad <shivam.52.nishad@gmail.com>
2025-03-11 02:40:58 +0530

    fix

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://localhost:5000||https://revoeai-backend-dbt5.onrender.com",
+  baseURL: "https://revoeai-backend-dbt5.onrender.com",
   withCredentials: true, // ✅ Important for CORS with cookies
   headers: {
     "Content-Type": "application/json",
